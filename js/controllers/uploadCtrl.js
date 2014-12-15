b6039563aa5a1944a783e9f71a41792c14348bd3
Vlad Sternbach <gr82bu@gmail.com>
2014-12-15 18:28:40 +0200

    wip

--- js/controllers/uploadCtrl.js
+++ js/controllers/uploadCtrl.js
@@ -1,8 +1,7 @@
 'use strict';
-angular.module('mbApp.controllers').controller('uploadCtrl', ['$scope', '$stateParams', '$modalInstance', 'fileStorage','$timeout', 'Facebook', function($scope, $stateParams, $modalInstance, fileStorage, $timeout, Facebook) {
+angular.module('mbApp.controllers').controller('uploadCtrl', ['$scope', '$document', '$modalInstance', 'fileStorage','$timeout', 'Facebook', function($scope, $document, $modalInstance, fileStorage, $timeout, Facebook) {
 
-	//$scope.file = {src: ''};
-	$scope.imgSrc = '';
+	$scope.img = {src: ''};
 
 	$scope.uploadFromPc = function () {
 		$timeout(function() {
@@ -10,37 +9,16 @@ angular.module('mbApp.controllers').controller('uploadCtrl', ['$scope', '$stateP
 		});
 	};
 
-	$scope.$watch('imgSrc', function (dataUrl) {
-		debugger;
-		if (dataUrl) {
-			angular.element('#imgPreview').src(dataUrl);
-		}
+	$scope.$watch('img.src', function (dataUrl) {
+		//debugger;
+		//if (dataUrl) {
+		//	var img = $document.getElementById("imgPreview");
+		//	img.onload = function () {
+		//		console.log(img.height);
+		//		debugger;
+		//	};
+        //
+		//}
 	});
 
-	//    debugger;
-	//    var d = document.getElementById("fileList");
-	//    if (!files.length) {
-	//        d.innerHTML = "<p>No files selected!</p>";
-	//    } else {
-	//        var list = document.createElement("ul");
-	//        d.appendChild(list);
-	//        for (var i = 0; i < files.length; i++) {
-	//            var li = document.createElement("li");
-	//            list.appendChild(li);
-	//
-	//            var img = document.createElement("img");
-	//            img.src = window.URL.createObjectURL(files[i]);
-	//            ;
-	//            img.height = 60;
-	//            img.onload = function () {
-	//                window.URL.revokeObjectURL(this.src);
-	//            }
-	//            li.appendChild(img);
-	//
-	//            var info = document.createElement("span");
-	//            info.innerHTML = files[i].name + ": " + files[i].size + " bytes";
-	//            li.appendChild(info);
-	//        }
-	//    }
-	//}
 }]);
